c00b8a6e95b307367ac1e179f235400db3a0423f
alex <leksfix@gmail.com>
2025-06-25 18:52:25 +0200

    Linter errors fixed

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,17 @@
+"""
+page_analyzer application
+"""
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route('/')
 def index():
+    """
+    Root route handler    
+    """
     pass
